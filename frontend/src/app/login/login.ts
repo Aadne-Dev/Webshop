fde6801f1c919b541a7758ad7a3fcf9524c60592
Aadne <aadne1999@live.no>
2025-11-17 18:55:28 +0100

    Added toast message and a rudimentary API connection to backend. Changed backend /health return to simply 200.

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from './login-api-call';
 
 @Component({
   selector: 'login-root',
@@ -8,9 +9,33 @@ import { Component, signal } from '@angular/core';
   styleUrl: './login.css'
 })
 export class Login {
+
+  // This is a temporary bit to showcase using snackBar toast messages, and connecting to the backend
+  constructor(private snackBar: MatSnackBar, private api: ApiService) {}
+  async apiCall() {
+    try {
+      this.api.getProducts().subscribe(data => {
+        if (data=="200") {
+          this.showToast("That password is already in use by: AdminRoot." );
+        }
+      });
+    } catch (error) {
+      this.showToast("API call failed" );
+    }
+  }
+  showToast(message: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 10000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top'
+    });
+  }
+
   alertVisible = signal(false);
 
   showAlert() {
     this.alertVisible.set(true);
   }
+
+
 }
